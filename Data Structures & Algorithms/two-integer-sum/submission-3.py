9bbf1a5ad7d2db9c67255542be34e02c49fbd578
hive-cmky <6779431+hive-cmky@users.noreply.github.com>
2026-04-16 21:44:33 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Dictionary to store: {value: index}
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                #return [index of complement, current index]
+                return [prevMap[diff], i]
+            prevMap[n] = i
